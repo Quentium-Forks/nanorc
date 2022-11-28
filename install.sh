3b32c3e4ad5bffadbfc9e2eb08a7ddd2fd6a7001
QuentiumYT <quentin.lienhardt@gmail.com>
2022-11-28 13:43:58 +0100

    New install script using local files

--- install.sh
+++ install.sh
@@ -1,53 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
-# check for unzip before we continue
-if [ ! "$(command -v unzip)" ]; then
-  echo 'unzip is required but was not found. Install unzip first and then run this script again.' >&2
-  exit 1
-fi
+_fetch_sources() {
+  # check for unzip before we continue
+  if [ ! "$(command -v unzip)" ]; then
+    echo 'unzip is required but was not found. Install unzip first and then run this script again.' >&2
+    exit 1
+  fi
 
-_fetch_sources(){
-  wget -O /tmp/nanorc.zip https://github.com/scopatz/nanorc/archive/master.zip
+  wget -qO /tmp/nanorc.zip https://github.com/Quentium-Forks/nanorc/archive/main.zip
   mkdir -p ~/.nano/
 
   cd ~/.nano/ || exit
-  unzip -o "/tmp/nanorc.zip"
-  mv nanorc-master/* ./
-  rm -rf nanorc-master
+  unzip -qo /tmp/nanorc.zip
+  mv nanorc-main/* ./
+  rm -rf nanorc-main
   rm /tmp/nanorc.zip
 }
 
-_update_nanorc(){
-  touch ~/.nanorc
-      
-  # add all includes from ~/.nano/nanorc if they're not already there
-  while read -r inc; do
-      if ! grep -q "$inc" "${NANORC_FILE}"; then
-          echo "$inc" >> "$NANORC_FILE"
-      fi
-  done < ~/.nano/nanorc
+_update_nanorc() {
+  cat nanorc > "$NANORC_FILE"
+
+  for file in *.nanorc; do
+    cp "$file" ~/.nano/
+    if ! grep -q "$file" "${NANORC_FILE}"; then
+      echo "include \"~/.nano/$file\"" >> "$NANORC_FILE"
+    fi
+  done
 }
 
-_update_nanorc_lite(){
+_update_nanorc_lite() {
   sed -i '/include "\/usr\/share\/nano\/\*\.nanorc"/i include "~\/.nano\/*.nanorc"' "${NANORC_FILE}"
 }
 
 NANORC_FILE=~/.nanorc
 
 case "$1" in
- -l|--lite)
-   UPDATE_LITE=1;;
- -h|--help)
-   echo "Install script for nanorc syntax highlights"
-   echo "Call with -l or --lite to update .nanorc with secondary precedence to existing .nanorc includes"
-   exit 0
- ;;
+-l | --lite)
+  UPDATE_LITE=1
+  ;;
+-h | --help)
+  echo "Install script for nanorc syntax highlights"
+  echo "Call with -l or --lite to update .nanorc with secondary precedence to existing .nanorc includes"
+  exit 0
+  ;;
 esac
 
-_fetch_sources;
-if [ $UPDATE_LITE ];
-then
+if [ $UPDATE_LITE ]; then
   _update_nanorc_lite
 else
+  _fetch_sources
   _update_nanorc
 fi
