e165c9b55cdf6c9e6c888302d338c004be2f85d3
QuentiumYT <quentin.lienhardt@gmail.com>
2023-12-20 10:38:24 +0100

    Refactor install.sh script to fix source install & improve readability

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_fetch_sources() {
+_fetch_repo() {
   # check for unzip before we continue
   if [ ! "$(command -v unzip)" ]; then
     echo 'unzip is required but was not found. Install unzip first and then run this script again.' >&2
@@ -10,38 +10,39 @@ _fetch_sources() {
   wget -qO /tmp/nanorc.zip https://github.com/Quentium-Forks/nanorc/archive/main.zip
   mkdir -p ~/.nano/
 
-  cd ~/.nano/ || exit
+  cd ~/.nano/
   unzip -qo /tmp/nanorc.zip
   mv nanorc-main/* ./
-  rm -rf nanorc-main
+
+  rm -rf nanorc-main/
   rm /tmp/nanorc.zip
 }
 
 _move_sources() {
   mkdir -p ~/.nano/
 
-  cp -r * ~/.nano/
+  cp -f *.nanorc ~/.nano/
 }
 
-_update_nanorc() {
+_reset_nanorc() {
   cat nanorc > "$NANORC_FILE"
+  echo "" >> "$NANORC_FILE"
 
   for file in *.nanorc; do
-    cp "$file" ~/.nano/
     if ! grep -q "$file" "${NANORC_FILE}"; then
       echo "include \"~/.nano/$file\"" >> "$NANORC_FILE"
     fi
   done
 }
 
-_update_nanorc_lite() {
+_update_nanorc_path() {
   sed -i '/include "\/usr\/share\/nano\/\*\.nanorc"/i include "~\/.nano\/*.nanorc"' "${NANORC_FILE}"
 }
 
 NANORC_FILE=~/.nanorc
 
 case "$1" in
--l | --lite)
+-p | --path)
   UPDATE_LITE=1
   ;;
 -s | --sources)
@@ -49,18 +50,18 @@ case "$1" in
   ;;
 -h | --help)
   echo "Install script for nanorc syntax highlights"
-  echo "Call with -l or --lite to update .nanorc with secondary precedence to existing .nanorc includes"
-  echo "Call with -s or --sources update .nanorc with local sources from this repository"
+  echo "Call with -p or --path to update the path of .nanorc files with secondary precedence to existing .nanorc includes"
+  echo "Call with -s or --sources to update .nanorc files with local sources from this repository"
   exit 0
   ;;
 esac
 
 if [ $UPDATE_LITE ]; then
-  _update_nanorc_lite
+  _update_nanorc_path
 elif [ $UPDATE_SOURCES ]; then
   _move_sources
-  _update_nanorc
+  _reset_nanorc
 else
-  _fetch_sources
-  _update_nanorc
+  _fetch_repo
+  _reset_nanorc
 fi
